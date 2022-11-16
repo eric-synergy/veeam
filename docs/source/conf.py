5c98519c308dccd23176b1e8804ff3b4b8ba5a04
Eric W <eric.wang@synergy-distribution.com>
2022-11-16 14:41:10 +0800

    test06

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Veeam'
+copyright = '2022, Eric W'
+author = 'Eric W'
 
 release = '0.1'
 version = '0.1.0'
