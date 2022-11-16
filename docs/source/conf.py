8e462990859d5af13e081ce259b8364a97111ffe
Eric W <eric.wang@synergy-distribution.com>
2022-11-16 15:40:53 +0800

    test00

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,49 +1,55 @@
 # Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
 
 project = 'Veeam'
-copyright = "2022 Eric W. All rights reserved."
+copyright = '2022, Eric W'
 author = 'Eric W'
 
+# The full version, including alpha/beta/rc tags
 release = '1.0'
-version = '1.0'
 
-# -- General configuration
 
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-    "sphinxemoji.sphinxemoji", 
-    "sphinx_contributors", 
-    "sphinx_copybutton"
 ]
 
-sphinxemoji_style = 'twemoji'
-
-# intersphinx_mapping = {
-#     'python': ('https://docs.python.org/3/', None),
-#     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-# }
-# intersphinx_disabled_domains = ['std']
-
+# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
-# -- Options for HTML output
 
-html_title = "V Demo"
-html_theme_options = {}
-html_theme_options["announcement"] = (
-        " Welcome to V Lab "
 
-)
+# -- Options for HTML output -------------------------------------------------
 
-#html_theme = 'sphinx_rtd_theme'
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'alabaster'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-# -- Options for EPUB output
-
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,25 +1,23 @@
-Welcome to V Lab!
-===================================
+.. Veeam documentation master file, created by
+   sphinx-quickstart on Wed Nov 16 15:38:29 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
 
-**V Lab** is for sharing Data Backup knowledge.
-`V Website <https://www.veeam.com/>`_ 
-
-V is a *simple*, *fexible* and *reliable*.
-
-.. Please go to :doc:`introduction` section for further information, including
-.. how to :ref:`installation` the project.
-
-.. note::
-
-   This site is under constructing.
-
-Contents
---------
+Welcome to Veeam's documentation!
+=================================
 
 .. toctree::
    :maxdepth: 2
-   
-   introduction
-   labdesign
-   labdemo
-   feature
+   :caption: Contents:
+introduction
+labdesign
+labdemo
+feature
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
